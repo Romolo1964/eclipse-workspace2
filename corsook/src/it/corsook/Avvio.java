c02662ee7d2fa0b5df8bc1e048a4d091fdb933e3
Romolo1964 <fiorenzaromolo4@gmail.com>
2023-12-10 14:30:13 +0100

    Su Corso inserito due righe

--- corsook/src/it/corsook/Avvio.java
+++ corsook/src/it/corsook/Avvio.java
@@ -1,12 +1,14 @@
-package it.prova;
+package it.corsook;
 
 public class Avvio {
 
+	public Avvio() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("CIAO");
-		System.out.println("CIAO");
-		System.out.println("CIAO");
+
 	}
 
 }
